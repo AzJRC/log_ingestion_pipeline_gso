326a229318cbbac307f101205cd23bb11dc5dbb9
AzJRC <rodajrc@gmail.com>
2025-06-30 10:09:12 -0600

    WMT: Build-QueryMetadata1.0 - Small changes
    - Refactor: Declaration of $RootDatabase variable
    - Addition: Added $EvtMeta and Parse-CommandBlockEventMetadata statements for future use
    - Addition: Import Utilities
    - Addition: Declared function QueryEventMetadata for future development

--- wef/WefManagementTool/modules/Build-QueryMetadata1.0.ps1
+++ wef/WefManagementTool/modules/Build-QueryMetadata1.0.ps1
@@ -1,8 +1,11 @@
 
-# Build-MetadataQuery follows QUERY.XML schema V1.0
+using namespace system.collections.generic
+
+
+# Build-MetadataQuery follows the QUERY.XML propietary schema META-1.0
 function Build-MetadataQuery {
     param(
-        [string]$RootDatabase = (Join-Path $PSScriptRoot "..\\QueriesDB" | Resolve-Path)
+        [string]$RootDatabase = (Join-Path -Path $PSScriptRoot -ChildPath '..\QueriesDB' | Resolve-Path)
     )
 
     $XmlQueryFiles = Get-ChildItem -Path $RootDatabase -Recurse -Filter "*.query.xml" -File
@@ -11,7 +14,8 @@ function Build-MetadataQuery {
         $RawXmlLines = Get-Content $XmlQueryFile.FullName
 
         # 1. Parse comment block metadata
-        $Meta = Parse-CommentBlock -Lines $RawXmlLines
+        $Meta = Parse-CommentBlockMetadata -Lines $RawXmlLines
+        $EvtMeta = Parse-CommentBlockEventMetadata -Lines $RawXmlLines  # [Unused]
 
         # 2. Parse <Query> elements
         [xml]$Xml = $RawXmlLines -join "`n"
@@ -36,5 +40,48 @@ function Build-MetadataQuery {
         # 4. Write META.JSON
         $OutputJsonPath = $XmlQueryFile.FullName -replace "query.xml", "meta.json"
         $JsonObject | ConvertTo-Json -Depth 10 | Out-File $OutputJsonPath -Encoding UTF8
+    }
+}
+
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Utilities\Get-QuerySchemaElements.ps1')
+# class QueryTypeElement {}
+# class QueryElement {}
+# class QUeryListElement {}
+
+
+
+class QueryEventMetadata {
+    static [string] $SCHEMA_EVT_VERSION = 1.0
+
+    # [TODO]
+}
+
+class QueryMetaIntent {
+
+    hidden [ValidateNotNullOrEmpty()][string]$Primary   # Unique, mandatory
+    hidden [list[string]]$Secondary                     # Non-unique, optional
+
+    [void] SetIntent([string]$PrimaryIntent, [list[string]]$SecondaryIntent) {
+        $this.Primary = $PrimaryIntent
+        if ($SecondaryIntent.Count -gt 0) { $this.Secondary = $SecondaryIntent }
+    }
+}
+
+class QueryMetadata {
+    static [string] $SCHEMA_META_VERSION = 1.0
+
+    # Mandatory fields
+    [ValidateNotNullOrEmpty()][string]$QueryName
+    [ValidateNotNullOrEmpty()][QueryMetaIntent]$Intent
+
+}
+
+function Parse-CommentBlockMetadata {
+    param(
+        [list]$Lines
+    )
+
+    $Lines | ForEach-Object {
+        
     }
 }
