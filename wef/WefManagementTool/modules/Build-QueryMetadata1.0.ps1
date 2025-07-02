eaf568c5c1ee592d078052b7dda73c37f10c2e00
Alejandro Javier Rodríguez Cantón <rodajrc@gmail.com>
2025-07-02 10:18:29 -0600

    Build Query Metadata and Query Metadata Schema 1.0 (#11)
    * Clean: Remove Files\ folder
    
    * Clean: Remove duplicate .gitignore
    
    * WMT: Build-QueryMetadata Small Refactor
    
    - $RootDatabase declaration more robust
    
    * WMF: Query XML Schema Documentation
    
    - Added new documentation for the QUERY.XML files
    - Standardization of QUERY.XML files
    
    * WMT: Start developing Bulid-QueryMetadata1.0
    
    - New Build-QueryMetadata script for better performance and architecture
    
    * WMT: Build-QueryMetadata1.0 - Small changes
    
    - Refactor: Declaration of $RootDatabase variable
    - Addition: Added $EvtMeta and Parse-CommandBlockEventMetadata statements for future use
    - Addition: Import Utilities
    - Addition: Declared function QueryEventMetadata for future development
    
    * WMF: Doc - QUERY_XML_SCHEMA.md auditpol mapping
    
    - Added information about Microsoft`s auditpol event categories and
      suggested mapping for Intent.Primary
    
    * WMT: File Sharing Query XML - Small changes
    
    - Added supported platorms for the query
    - Fixed small typos and unproper syntax
    
    * WMT: Docs - QUERY_XML_SCHEMA
    
    - Descriptions of fields refined
    
    * WMT: Docs - QUERY_XML_SCHEMA
    
    - Fixed QueryDate incorrect documentation about format of the date
    
    * WMT: Query - Added Kerberos Auditing Query
    
    - New Keberos Auditing query for Kerberoasting detection
    - Final tweeks in QUERY_XML_SCHEMA.md
    - Minor changes in file_sharing_activity.query.xml
    
    * WMT: Build-QueryMetadata1.0 - Added class declarations
    
    - Addition: Class declaration for the MetadataSchema
    
    * WMT: Cleaning - Deleted unwanted files from QueriesDB
    
    - Deleted META.JSON files. These can be regerenerated later with the
      Build-QueryMetadata script
    
    * WMT: Docs - Added new fields in Query XML Schema
    
    - Added Verbosity field
    - Added Requirements field
    - Added still under development RequiredSettings subfields
    
    * WMT: Doc/Query
    
    Doc - QUERY_XML_SCHEMA.md
    - Added new syntax definitions for comments
    
    Query - Powershell Activity
    - Refactored query with powershell monitoring events
    
    * WMT: Query - Windows Logon Activity
    
    - Refactor of Windows Logon Activity Query
    - MetaSchema V1.0 compliant
    
    * WMT: Doc/Query
    
    Doc: QUERY_XML_SCHEMA.md
    - Added documentation about the new coverage/complementary feature of QUERY.XML files
    
    Query: Windows Logon Activity
    - Merged windows logon activity and privileged windows logon activity
      into one complementary QUERY.XML file
    
    * WMT - Queries: New Normalized queries to MetaSchema 1.0
    
    - PowerShell Activity Query
    - WMI Activity Query Normalized
    - Account Management Query Normalized
    - File Sharing Activity Normalized
    - Windows Logon Activity
    - Traffic Flow
    
    * WMT: Clean and Query
    
    Cleaning
    - Renamed documentation files with better names for consistency
    
    Query
    - Small change: Added 'Windows Sysmon` in source tag
    
    * WMT: Get-WMTQueryMetadataSchema.ps1
    
    - Added new comprehensive utility file based on QUERYMETA_SCHEMA.md 1.0
      for programmability of future scripts.
    
    * WMT: Doc - QUERYNETA_SCHEMA.md
    
    - Clean and reformulated aspects about coverage and complementary
      queries
    
    * WMT: Doc and Query small changes
    
    - Changed META EVT SCHEMA for comments.
    
    * WMT: Build-QueryMetadata1.0
    
    - Added required enum declarations in Get-WMTQueryMetadataSchema
    - Added MetaSchemaVersion validation in Build-QueryMetadata1.0
    
    * WMT: Get-WMTQueryMetadataSchema
    
    - Added empty constructor for easier declaration of a
      QueryMetadataSchema object without needing to parse QUERY.XML files
      previously.

--- wef/WefManagementTool/modules/Build-QueryMetadata1.0.ps1
+++ wef/WefManagementTool/modules/Build-QueryMetadata1.0.ps1
@@ -1,25 +1,44 @@
 
 using namespace system.collections.generic
 
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Utilities\Get-MicrosoftQueryElementSchema.ps1')
+# class QueryTypeElement {}
+# class QueryElement {}
+# class QueryListElement {}
 
-# Build-MetadataQuery follows the QUERY.XML propietary schema META-1.0
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Utilities\Get-WMTQueryMetadataSchema.ps1')
+# enum MetaSchemaIntentFields
+# enum MetaSchemaAuthorFields
+# enum MetaSchemaFields
+# class QueryMetadataSchema {}
+
 function Build-MetadataQuery {
     param(
         [string]$RootDatabase = (Join-Path -Path $PSScriptRoot -ChildPath '..\QueriesDB' | Resolve-Path)
     )
 
-    $XmlQueryFiles = Get-ChildItem -Path $RootDatabase -Recurse -Filter "*.query.xml" -File
+    $queryXmlFiles = Get-ChildItem -Path $RootDatabase -Recurse -Filter "*.query.xml" -File
+    foreach ($queryXmlFile in $queryXmlFiles) {
+        $RawQueryXmlLines = Get-Content $queryXmlFile.FullName
+        [xml]$QueryXml = "<?xml version=`"1.0`" encoding=`"utf-8`"?>`n$RawQueryXmlLines"
 
-    foreach ($XmlQueryFile in $XmlQueryFiles) {
-        $RawXmlLines = Get-Content $XmlQueryFile.FullName
+        # 0. Validate Schema Version
+        $validQueryXml = $false
+        $match = $RawQueryXmlLines | Where-Object {
+            # The cryptic script ($_ -split ':', 2)[1].Trim() is the value '1.0' in the string 'MetaSchemaVersion: 1.0'
+            $_ -match 'MetaSchemaVersion' -and (($_ -split ':', 2)[1].Trim() -eq [QueryMetadataSchema]::SCHEMA_META_VERSION)
+        } | Select-Object -First 1
+        if ($match) { $validQueryXml = $true }
 
-        # 1. Parse comment block metadata
-        $Meta = Parse-CommentBlockMetadata -Lines $RawXmlLines
-        $EvtMeta = Parse-CommentBlockEventMetadata -Lines $RawXmlLines  # [Unused]
+        if (-not $validQueryXml) {
+            throw "[-] Invalid QueryXmlFile MetaSchemaVersion. Supported version is: $([QueryMetadataSchema]::SCHEMA_META_VERSION)"
+        }
 
-        # 2. Parse <Query> elements
-        [xml]$Xml = $RawXmlLines -join "`n"
-        $QueryElements = Parse-QueryXmlElements -Xml $Xml
+        # 1. Parse <Query> elements
+        $QueryElements = Parse-QueryXmlElements -Xml $QueryXml
+
+        # 2. Parse comment block metadata
+        $Meta = Parse-QueryMetadata -Lines $RawQueryXmlLines
 
         # 3. Compose final metadata object
         $JsonObject = [ordered]@{
@@ -38,50 +57,8 @@ function Build-MetadataQuery {
         }
 
         # 4. Write META.JSON
-        $OutputJsonPath = $XmlQueryFile.FullName -replace "query.xml", "meta.json"
-        $JsonObject | ConvertTo-Json -Depth 10 | Out-File $OutputJsonPath -Encoding UTF8
-    }
-}
-
-Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Utilities\Get-QuerySchemaElements.ps1')
-# class QueryTypeElement {}
-# class QueryElement {}
-# class QUeryListElement {}
-
-
-
-class QueryEventMetadata {
-    static [string] $SCHEMA_EVT_VERSION = 1.0
-
-    # [TODO]
-}
-
-class QueryMetaIntent {
-
-    hidden [ValidateNotNullOrEmpty()][string]$Primary   # Unique, mandatory
-    hidden [list[string]]$Secondary                     # Non-unique, optional
-
-    [void] SetIntent([string]$PrimaryIntent, [list[string]]$SecondaryIntent) {
-        $this.Primary = $PrimaryIntent
-        if ($SecondaryIntent.Count -gt 0) { $this.Secondary = $SecondaryIntent }
-    }
-}
-
-class QueryMetadata {
-    static [string] $SCHEMA_META_VERSION = 1.0
-
-    # Mandatory fields
-    [ValidateNotNullOrEmpty()][string]$QueryName
-    [ValidateNotNullOrEmpty()][QueryMetaIntent]$Intent
-
-}
-
-function Parse-CommentBlockMetadata {
-    param(
-        [list]$Lines
-    )
-
-    $Lines | ForEach-Object {
-        
+        continue    # [Stop] Temporary 
+        $OutputJsonPath = $queryXmlFile.FullName -replace "query.xml", "meta.json"
+        $JsonObject | ConvertTo-Json -Depth 5 | Out-File $OutputJsonPath -Encoding UTF8
     }
 }
