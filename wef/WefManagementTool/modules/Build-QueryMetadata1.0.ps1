e61f749c550862bd2a258db022f17a732afdb1a2
AzJRC <rodajrc@gmail.com>
2025-07-01 21:49:53 -0600

    WMT: Get-WMTQueryMetadataSchema.ps1
    - Added new comprehensive utility file based on QUERYMETA_SCHEMA.md 1.0
      for programmability of future scripts.

--- wef/WefManagementTool/modules/Build-QueryMetadata1.0.ps1
+++ wef/WefManagementTool/modules/Build-QueryMetadata1.0.ps1
@@ -1,8 +1,9 @@
 
 using namespace system.collections.generic
 
+$SUPPORTED_SCHEMA_VERSION = 1.0
+$SUPPORTED_EVENT_SCHEMA_VERSION = 1.0
 
-# Build-MetadataQuery follows the QUERY.XML propietary schema META-1.0
 function Build-MetadataQuery {
     param(
         [string]$RootDatabase = (Join-Path -Path $PSScriptRoot -ChildPath '..\QueriesDB' | Resolve-Path)
@@ -14,8 +15,7 @@ function Build-MetadataQuery {
         $RawXmlLines = Get-Content $XmlQueryFile.FullName
 
         # 1. Parse comment block metadata
-        $Meta = Parse-CommentBlockMetadata -Lines $RawXmlLines
-        $EvtMeta = Parse-CommentBlockEventMetadata -Lines $RawXmlLines  # [Unused]
+        $Meta = Parse-QueryMetadata -Lines $RawXmlLines
 
         # 2. Parse <Query> elements
         [xml]$Xml = $RawXmlLines -join "`n"
@@ -43,70 +43,14 @@ function Build-MetadataQuery {
     }
 }
 
-Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Utilities\Get-QuerySchemaElements.ps1')
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Utilities\Get-MicrosoftQueryElementSchema.ps1')
 # class QueryTypeElement {}
 # class QueryElement {}
-# class QUeryListElement {}
+# class QueryListElement {}
 
-
-
-class QueryEventMetadataSchema {
-    static [string] $SCHEMA_EVT_VERSION = 1.0
-
-    # [TODO]
-}
-
-class IntentField {
-
-    hidden [ValidateNotNullOrEmpty()][string]$Primary   # Unique, mandatory
-    hidden [list[string]]$Secondary                     # Non-unique, optional
-
-    [void] SetIntent([string]$PrimaryIntent, [list[string]]$SecondaryIntent) {
-        $this.Primary = $PrimaryIntent
-        if ($SecondaryIntent.Count -gt 0) { $this.Secondary = $SecondaryIntent }
-    }
-}
-
-
-class ResourceField {
-    hidden [string]$ResourceKey
-    hidden [string]$ResourceValue
-
-    [void] ParseAndSetResource($InputResource) {
-        $Key, $value = $InputResource -split '/'
-        if (-not ($key -and $value)) { Write-Error "Invalid input for ResourceField.ParseResource: $InputResource"}
-        $this.ResourceKey = $key
-        $this.ResourceValue = $Value
-    }
-
-    [void] SetResourceKey($InputKey) {
-        $this.ResourceKey = $InputKey
-    }
-
-    [void] SetResourceValue($InputValue) {
-        $this.ResourceValue = $InputValue
-    }
-}
-
-class QueryMetaAuthorField {
-
-    hidden [string]$AuthorName
-    hidden [list[string]]$AuthorAlias
-    hidden [list[ResourceField]]$Resource
-
-}
-
-class QueryMetadataSchema {
-    static [string] $SCHEMA_META_VERSION = 1.0
-
-    # Mandatory fields
-    [ValidateNotNullOrEmpty()][string]$QueryName
-    [ValidateNotNullOrEmpty()][IntentField]$Intent
-    [ValidateNotNullOrEmpty()][QueryMetaAuthorField]$AuthorField
-
-}
-
-function Parse-CommentBlockMetadata {
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Utilities\Get-WMTQueryMetadataSchema.ps1')
+# class QueryMetadataSchema {}
+function Parse-QueryMetadata {
     param(
         [list]$Lines
     )
